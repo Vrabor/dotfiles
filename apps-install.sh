edcac501ee5414c8561bd921c6dda7d1410f9ec6
Vrabor <37751734+Vrabor@users.noreply.github.com>
2019-04-14 22:07:13 +0200

    Add files via upload

---
+++ apps-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+sudo pacman -S pcmanfm-qt --noconfirm --needed
+sudo pacman -S rxvt-unicode --noconfirm --needed
+sudo pacman -S firefox --noconfirm --needed
+sudo pacman -S clang --noconfirm --needed
+sudo pacman -S git --noconfirm --needed
+sudo pacman -S nitrogen --noconfirm --needed
+sudo pacman -S pix --noconfirm --needed
+sudo pacman -S pypy --noconfirm --needed
+sudo pacman -S python-pip --noconfirm --needed
+sudo pacman -S python-matplotlib --noconfirm --needed
+sudo pacman -S ranger --noconfirm --needed
+sudo pacman -S julia --noconfirm --needed
+sudo pacman -S neovim --noconfirm --needed
+sudo pacman -S code --noconfirm --needed
+sudo pacman -S vlc --noconfirm --needed
+sudo pacman -S okular --noconfirm --needed
+sudo pacman -S hexchat --noconfirm --needed
+sudo pacman -S jdk-openjdk --noconfirm --needed
+sudo pacman -S gparted --noconfirm --needed
+sudo pacman -S htop --noconfirm --needed
+sudo pacman -S hardinfo --noconfirm --needed
+sudo pacman -S lm_sensors --noconfirm --needed
+sudo pacman -S urxvt-perls --noconfirm --needed
+sudo pacman -S --noconfirm --needed unace unrar unzip
+
+
